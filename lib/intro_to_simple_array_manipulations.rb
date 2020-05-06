7bda7d78c4ceba4ea8f71c89b6f6a3dcca4c2a0d
Travis Prol <travisjohnprol@gmail.com>
2020-05-06 21:36:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,6 +2,6 @@ def using_concat(array1, array2)
   array1.concat array2
 end
 
-def using_insert(array, eleement)
-  array.insert(4, element)
+def using_insert(array, element)
+  array.insert(3, element)
 end
